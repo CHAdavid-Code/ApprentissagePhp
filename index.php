5f7c9903e9183c8675e17cea662e49657378afc0
root <root@DESKTOP-6039N2C.localdomain>
2021-09-01 15:22:55 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+<title>David CHAING</title>
+  </head>
+  <body>
+  <?php include("./partial/_navBar.php"); ?>
+  <div class="container">
+
+    <h1>Le site de David CHAING</h1>
+</div>
+    <script src="/js/bootstrap.bundle.min.js"></script>
+</body>
+    </html>
---
+++ partial/_navBar.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">David CHAING</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="/index.php">Accueil
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/PageTest.php">test</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/exercices.php">Exo</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/notes.php">notes</a>
+        </li>
+        
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
